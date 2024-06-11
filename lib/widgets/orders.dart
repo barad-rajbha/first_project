76e3ca09e7eecffe52ad6f8056c762221c91bb04
Rajbha <baradrajbha!@gmail.com>
2024-06-11 15:37:14 +0530

    11-06-2024

--- lib/widgets/orders.dart
+++ lib/widgets/orders.dart
@@ -13,90 +13,92 @@ class _OrdersState extends State<Orders> {
     double displaywidth = MediaQuery.of(context).size.height;
     return Card(
       elevation: 5,
-      child: Row(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          SizedBox(
-            width: displaywidth * 0.015,
-          ),
-          Container(
-            height: displaywidth * 0.150,
-            width: displaywidth * 0.100,
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                image: AssetImage('asset/img/1.png'),
+      child: Container(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: displaywidth * 0.015,
+            ),
+            Container(
+              height: displaywidth * 0.150,
+              width: displaywidth * 0.100,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('asset/img/1.png'),
+                ),
               ),
             ),
-          ),
-          SizedBox(
-            width: 15,
-          ),
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                SizedBox(
-                  height: 5,
-                ),
-                Text(
-                  'Puma Shoes',
-                  style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
-                ),
-                Text(
-                  '📍 dr.yagnik road rajkot,360001\n⏱️ 03 jan 2022',
-                  style: TextStyle(fontSize: 15),
-                ),
-                SizedBox(
-                  height: 5,
-                ),
-                Row(
-                  children: [
-                    Card(
-                      elevation: 5,
-                      child: Container(
-                        height: displaywidth * 0.040,
-                        width: displaywidth * 0.120,
-                        child: Center(
-                            child: Text(
-                          'Accept',
-                          style: TextStyle(
-                              color: Colors.white,
-                              letterSpacing: 1,
-                              fontSize: 15),
-                        )),
-                        decoration: BoxDecoration(
-                            color: Colors.green,
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                    ),
-                    SizedBox(
-                      width: 10,
-                    ),
-                    Card(
-                      elevation: 5,
-                      child: Container(
-                        height: displaywidth * 0.040,
-                        width: displaywidth * 0.120,
-                        child: Center(
-                            child: Text(
-                          'Reject',
-                          style: TextStyle(
-                              color: Colors.white,
-                              letterSpacing: 1,
-                              fontSize: 15),
-                        )),
-                        decoration: BoxDecoration(
-                            color: Colors.red,
-                            borderRadius: BorderRadius.circular(10)),
-                      ),
-                    ),
-                  ],
-                )
-              ],
+            SizedBox(
+              width:  displaywidth * 0.015,
             ),
-          )
-        ],
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height:  displaywidth * 0.005,
+                  ),
+                  Text(
+                    'Puma Shoes',
+                    style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                  ),
+                  Text(
+                    '📍 dr.yagnik road rajkot, 360001\n⏱️ 03-jan-2022',
+                    style: TextStyle(fontSize: 15),
+                  ),
+                  SizedBox(
+                    height:  displaywidth * 0.005,
+                  ),
+                  Row(
+                    children: [
+                      Card(
+                        elevation: 5,
+                        child: Container(
+                          height: displaywidth * 0.040,
+                          width: displaywidth * 0.110,
+                          child: Center(
+                              child: Text(
+                            'Accept',
+                            style: TextStyle(
+                                color: Colors.white,
+                                letterSpacing: 1,
+                                fontSize: 15),
+                          )),
+                          decoration: BoxDecoration(
+                              color: Colors.green,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                      SizedBox(
+                        width:  displaywidth * 0.010,
+                      ),
+                      Card(
+                        elevation: 5,
+                        child: Container(
+                          height: displaywidth * 0.040,
+                          width: displaywidth * 0.110,
+                          child: Center(
+                              child: Text(
+                            'Reject',
+                            style: TextStyle(
+                                color: Colors.white,
+                                letterSpacing: 1,
+                                fontSize: 15),
+                          )),
+                          decoration: BoxDecoration(
+                              color: Colors.red,
+                              borderRadius: BorderRadius.circular(10)),
+                        ),
+                      ),
+                    ],
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
